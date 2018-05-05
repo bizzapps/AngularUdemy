f495ce5061e51cbeebe6891941115e98f074f70c
ezyapps <engr.nayan@gmail.com>
2018-05-05 23:30:33 +0600

    commit on 05-May-2018

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  showFeature: string;
+  onNavigate(feature) {
+    this.showFeature = feature;
+  }
 }
